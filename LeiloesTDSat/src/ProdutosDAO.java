67d5a3547c62cdadb045c81522e1ea9445612cf1
Caio David <dcaio1218@gmail.com>
2026-02-24 22:41:19 -0300

    Implementado metodo listarProdutosVendidos no ProdutosDAO

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -85,6 +85,37 @@ public class ProdutosDAO {
         JOptionPane.showMessageDialog(null, "Erro ao vender produto: " + e.getMessage());
     }
 }
+public ArrayList<ProdutosDTO> listarProdutosVendidos() {
+
+    String sql = "SELECT * FROM produtos WHERE status = 'Vendido'";
+    ArrayList<ProdutosDTO> lista = new ArrayList<>();
+
+    try {
+
+        conn = new conectaDAO().connectDB();
+        prep = conn.prepareStatement(sql);
+        resultset = prep.executeQuery();
+
+        while (resultset.next()) {
+
+            ProdutosDTO produto = new ProdutosDTO();
+
+            produto.setId(resultset.getInt("id"));
+            produto.setNome(resultset.getString("nome"));
+            produto.setValor(resultset.getInt("valor"));
+            produto.setStatus(resultset.getString("status"));
+
+            lista.add(produto);
+        }
+
+        conn.close();
+
+    } catch (Exception e) {
+        JOptionPane.showMessageDialog(null, "Erro ao listar vendidos: " + e.getMessage());
+    }
+
+    return lista;
+}
 
     
         
